c49ff25716269ef9e81d0cbe63f18c4c38b8fd99
Paulo <paulogrdc@gmail.com>
2024-05-20 19:55:00 -0300

    modifiquei o codigo da chamada api

--- index.js
+++ index.js
@@ -1,34 +1,28 @@
-
 const formPesquisa = document.querySelector('form')
-apiKey = ''
+apiKey = '786648c2'
+
 formPesquisa.onsubmit = (ev) => {
     ev.preventDefault()
-
-
     const pesquisa = ev.target.pesquisa.value
 
-    if(pesquisa =="") {
+    if(pesquisa ==="") {
         alert('Campo invalido!!')
     } else {
-        fetch(`http://www.omdbapi.com/?apikey=${apiKey}]&s=${pesquisa}        `)
+        fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s=${pesquisa}`)
         .then(res => res.json())
-        .then(json => console.log(json))
-    } 
-    const carregarLista = (json) => {
-        const lista = document.querySelector("div.lista")
-        lista.innerHTML = ""
+        .then(json => carregarLista(json))
+    }   
+}
 
-        json.search.forEach(element => {
-            console.log(element)
+const carregarLista = (json) => {
+    const lista = document.querySelector("div.lista");
+    lista.innerHTML = "";
 
-            let item = document.createElement('div')
-            item.classList.add('item')
-    
-            item.innerHTML = ` <h2> ${element.Title}</h2><img src="${element.Poster}"/>`
-    
-            lista.appendChild(item)
+        json.Search.forEach(element => {
+            let item = document.createElement('div');
+            item.classList.add('item');
+            item.innerHTML = `<h2>${element.Title}</h2><img src="${element.Poster}"/>`;
+            lista.appendChild(item);
         });
 
-
-    }
-}
+};
--- node_ee15a1f81898d.tmp.js
+++
@@ -1,4 +0,0 @@
-fetch('https://api.themoviedb.org/3/movi/11')
- .then((res) => res.json())
- .then((date) => console.log(date))
- .catch((error) => console.error('Error', error))
