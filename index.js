6917ca2e0f711ff2d180814c878a45da14ffff74
Paulo <paulogrdc@gmail.com>
2024-06-12 09:43:34 -0300

    fiz o header para o site

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
+const search = document.getElementById('search')
+
+search.addEventListener('click', () => {
+    let input = document.createElement('input')
+    input.setAttribute('type', 'text')
+    input.setAttribute('name', 'pesquisa')
+    input.setAttribute('id', 'input')
+    input.setAttribute('placeholder', 'series,filmes')
+
+    let form = document.getElementById('form')
+    form.appendChild(input)
+  
+})
+
+
+
+
+
 const formPesquisa = document.querySelector('form')
 apiKey = '786648c2'
 
 formPesquisa.onsubmit = (ev) => {
+    
     ev.preventDefault()
     const pesquisa = ev.target.pesquisa.value
 
@@ -17,12 +36,16 @@ formPesquisa.onsubmit = (ev) => {
 const carregarLista = (json) => {
     const lista = document.querySelector("div.lista");
     lista.innerHTML = "";
+    
 
         json.Search.forEach(element => {
             let item = document.createElement('div');
             item.classList.add('item');
             item.innerHTML = `<img src="${element.Poster}"/>`;
             lista.appendChild(item);
+
         });
 
+
 };
+
